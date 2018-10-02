774e324ebd8acd9160d093e721509f2e0b722e87
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-10-02 15:01:43 -0500

    I_Do_Declare

---
+++ Assignments/JavaScript/I_Do_Declare.js
@@ -0,0 +1,29 @@
+// 4 Numbers
+var num1 = 7;
+var num2 = -7;
+var num3 = 3.14;
+var num4 = 1000000000;
+
+// 3 Strings
+var string1 = "Kevin"
+var string2 = "Camp"
+var string3 = "Hi there! How are you?"
+
+// 2 Booleans
+var boolean1 = true;
+var boolean2 = false;
+
+// 1 Undefined Variable
+var temp;
+
+// Print Results to console
+console.log("First Number:", num1);
+console.log("Second Number:", num2);
+console.log("Third Number:", num3);
+console.log("Fourth Number:", num4);
+console.log("First String:", string1);
+console.log("Second String:", string2);
+console.log("Third String:", string3);
+console.log("First Boolean:", boolean1);
+console.log("Second Boolean:", boolean2);
+console.log("Undefined Variable:", temp);
