74d14728b2b9566833c8353405a1c121e82ebf3a
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-10-02 20:58:27 -0500

    While_You_Wait beautify

--- Assignments/JavaScript/While_You_Wait.js
+++ Assignments/JavaScript/While_You_Wait.js
@@ -1,14 +1,14 @@
 var daysUntilBirthday = 60;
 var i = daysUntilBirthday;
 
-for(var i = daysUntilBirthday; i > 30 && i; i --){
+for (var i = daysUntilBirthday; i > 30 && i; i--) {
   console.log(i + "days until my birthday. Such a long time... :(");
 }
-while(i <= 30 && i > 5){
+while (i <= 30 && i > 5) {
   console.log(i + "days until my birthday. Getting closer!");
-  i --;
+  i--;
 }
-for(i; i > 0; i --){
+for (i; i > 0; i--) {
   console.log(i + "DAYS UNTIL MY BIRTHDAY!!!");
 }
 
