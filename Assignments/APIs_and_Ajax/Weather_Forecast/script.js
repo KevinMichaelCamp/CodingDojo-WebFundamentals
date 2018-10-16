f9a0fe21693dd23202bdbb224c031538120fc62a
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-10-16 17:19:36 -0500

    Weather_Forecast

---
+++ Assignments/APIs_and_Ajax/Weather_Forecast/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+  $('form').submit(function(){
+    return false;
+  })
+
+  $('button').click(function(){
+    var city = $('input').val();
+    console.log(city);
+    $.get("http://api/openweathermap.org/data/2.5/weather?q=" + city + "/5b1a0de273bbbcbbe13f59305c5a3d21", function(res){
+      console.log(res);
+    }, 'json');
+  })
+
+})
