9f2d434b81e218d67891dc9f368050c100ddec73
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-10-16 21:31:58 -0500

    Weather_Forecast

--- Assignments/APIs_and_Ajax/Weather_Forecast/script.js
+++ Assignments/APIs_and_Ajax/Weather_Forecast/script.js
@@ -7,7 +7,11 @@ $(document).ready(function() {
   $('button').click(function() {
     var city = $('input').val();
     console.log(city);
-    $.get("http://api/openweathermap.org/data/2.5/weather?q=" + city + "/5b1a0de273bbbcbbe13f59305c5a3d21", function(res) {
+    $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=8a9feb7c238bb86175a7ffbfb74c1356", function(res) {
+      var degrees = Math.round(((9/5) * res.main.temp) - 459.67);
+      var weather = res.weather[0].description;
+      $('#results').html("<h2>The weather in "+city+" is "+weather+".")
+      $('#results').append("<h2>The temperature is "+degrees+"° Fahrenheit.</h2>")
       console.log(res);
     }, 'json');
   })
