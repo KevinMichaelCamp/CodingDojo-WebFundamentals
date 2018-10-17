bc81ff36639068b6aa182a30531c06e0669ae33b
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-10-16 22:31:53 -0500

    Game_of_Thrones Validated and Beautified

--- Assignments/APIs_and_Ajax/Game_of_Thrones/script.js
+++ Assignments/APIs_and_Ajax/Game_of_Thrones/script.js
@@ -1,35 +1,35 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-  $('#stark').click(function(){
-    $.get("https://www.anapioficeandfire.com/api/houses/362", function(res){
-      $('#detail').html("<p>Name: "+res.name+"</p>");
-      $('#detail').append("<p>Words: "+res.words+"</p>");
-      $('#detail').append("<p>Titles: "+res.titles+"</p>");
+  $('#stark').click(function() {
+    $.get("https://www.anapioficeandfire.com/api/houses/362", function(res) {
+      $('#detail').html("<p>Name: " + res.name + "</p>");
+      $('#detail').append("<p>Words: " + res.words + "</p>");
+      $('#detail').append("<p>Titles: " + res.titles + "</p>");
     }, 'json');
   })
 
-  $('#targaryen').click(function(){
-    $.get("https://www.anapioficeandfire.com/api/houses/378", function(res){
-      $('#detail').html("<p>Name: "+res.name+"</p>");
-      $('#detail').append("<p>Words: "+res.words+"</p>");
-      $('#detail').append("<p>Titles: "+res.titles+"</p>");
+  $('#targaryen').click(function() {
+    $.get("https://www.anapioficeandfire.com/api/houses/378", function(res) {
+      $('#detail').html("<p>Name: " + res.name + "</p>");
+      $('#detail').append("<p>Words: " + res.words + "</p>");
+      $('#detail').append("<p>Titles: " + res.titles + "</p>");
     }, 'json');
   })
 
-  $('#lannister').click(function(){
-    $.get("https://www.anapioficeandfire.com/api/houses/229", function(res){
-      $('#detail').html("<p>Name: "+res.name+"</p>");
-      $('#detail').append("<p>Words: "+res.words+"</p>");
-      $('#detail').append("<p>Titles: "+res.titles+"</p>");
+  $('#lannister').click(function() {
+    $.get("https://www.anapioficeandfire.com/api/houses/229", function(res) {
+      $('#detail').html("<p>Name: " + res.name + "</p>");
+      $('#detail').append("<p>Words: " + res.words + "</p>");
+      $('#detail').append("<p>Titles: " + res.titles + "</p>");
     }, 'json');
   })
 
-  $('#baratheon').click(function(){
-    $.get("https://www.anapioficeandfire.com/api/houses/16", function(res){
+  $('#baratheon').click(function() {
+    $.get("https://www.anapioficeandfire.com/api/houses/16", function(res) {
       console.log(res);
-      $('#detail').html("<p>Name: "+res.name+"</p>");
-      $('#detail').append("<p>Words: "+res.words+"</p>");
-      $('#detail').append("<p>Titles: "+res.titles+"</p>");
+      $('#detail').html("<p>Name: " + res.name + "</p>");
+      $('#detail').append("<p>Words: " + res.words + "</p>");
+      $('#detail').append("<p>Titles: " + res.titles + "</p>");
     }, 'json');
   })
 })
