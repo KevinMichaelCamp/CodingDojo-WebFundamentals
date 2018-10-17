43ce72111cffcfdf17187439ca61daa9d3bbdb93
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2018-10-17 15:24:57 -0500

    Date_Validations Validated & Beautified

--- Assignments/jQuery_UI/Date_Validations/script.js
+++ Assignments/jQuery_UI/Date_Validations/script.js
@@ -1,23 +1,22 @@
-$(document).ready(function(){
-  $('form').submit(function(){
+$(document).ready(function() {
+  $('form').submit(function() {
     return false;
   })
 
   $('#from').datepicker();
   $('#to').datepicker();
 
-  $('button').click(function(){
+  $('button').click(function() {
     var fromDate = $('#from').val();
     var toDate = $('#to').val();
     var name = $('#name').val();
     console.log(fromDate);
     console.log(toDate);
     console.log(name);
-    if(name == ""){
+    if (name == "") {
       alert("You Must Enter a Name!");
-    }
-    else{
-      $('#dialog').append("<p>Hi "+name+"! You are all set to leave on "+fromDate+" and return "+toDate+".")
+    } else {
+      $('#dialog').append("<p>Hi " + name + "! You are all set to leave on " + fromDate + " and return " + toDate + ".")
       $('#dialog').dialog();
     }
   })
